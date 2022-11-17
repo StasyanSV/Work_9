b61fa41aede762c03005b8fcd29c7c667dcec3ab
StasyanSV <stanislav_xx@mail.ru>
2022-11-17 11:06:21 +0400

    add Work_1

---
+++ Work_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2,"
+
+void NumbersChoice(int num)
+{
+    if (num <= 1) return;
+    else
+    {
+        if (num % 2 == 0)
+        {
+            Console.Write($"{num} ");
+            NumbersChoice(num - 1);
+        }
+        else NumbersChoice(num - 1);
+    }
+}
+
+
+Console.Write("Введите число N: ");
+int number = int.Parse(Console.ReadLine()!);
+NumbersChoice(number);
